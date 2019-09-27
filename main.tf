e20ff9a5b781f313455af2ff4194a41d92043ee7
bagy2017 <solt2309@stthomas.edu>
2019-09-26 21:27:31 -0700

    Added more files.

--- main.tf
+++ main.tf
@@ -23,3 +23,61 @@ resource "google_compute_instance" "svc-1" {
     }
   }
 }
+
+provisioner "remote-exec" {
+    connection {
+      host        = "${google_compute_instance.svc-1.network_interface.0.access_config.0.nat_ip}"
+      user        = "solt2309"
+      type        = "ssh"
+      private_key = "${file("~/.ssh/google_compute_engine")}"
+      }
+    inline = [
+      "mkdir -p ~/svc-01/html",
+    ]
+  }
+
+  provisioner "file" {
+    source      = "svc-01/"
+    destination = "~/svc-01"
+    connection {
+      host        = "${google_compute_instance.svc-1.network_interface.0.access_config.0.nat_ip}"
+      user        = "solt2309"
+      type        = "ssh"
+      private_key = "${file("~/.ssh/google_compute_engine")}"
+    }
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host        = "${google_compute_instance.svc-1.network_interface.0.access_config.0.nat_ip}"
+      user        = "solt2309"
+      type        = "ssh"
+      private_key = "${file("~/.ssh/google_compute_engine")}"
+      }
+    inline = [ 
+      "chmod +x ~/svc-01/install.sh",
+      "sudo ~/svc-01/install.sh",
+    ]
+  }
+
+  #service account is essential for file provisioner
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+
+}
+
+resource "google_compute_firewall" "default" {
+ name    = "svc01-firewall"
+ network = "default"
+
+ allow {
+   protocol = "tcp"
+   ports    = ["80"]
+ }
+}
+
+output "ip" {
+   value = "${google_compute_instance.svc-1.network_interface.0.access_config.0.nat_ip}"
+}
+
